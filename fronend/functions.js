5e44c2d7a33cf6e88f1324cc05746cded196a97f
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2022-03-12 15:27:01 +0200

    added fetch api

--- fronend/functions.js
+++ fronend/functions.js
@@ -1,11 +1,13 @@
-export async function take_temperatures()
-{
-    const array_for_temperatures = [];
+const array_for_temperatures = [];
 
+export async function takeParameters()
+{
     try
     {
         let result = await fetch('http://85.90.247.66/api/measurements-list/1');
+       // let result = await fetch(`http://85.90.247.66/api/measurements-list/${baseNumber}`);
         let data = await result.json();
+
 
         for(let i = data.length - 1; i >= 0; i--)
         {
@@ -15,13 +17,17 @@ export async function take_temperatures()
                 array_for_temperatures.unshift(fixed_temperatures);
             }
         }
-        
-        console.log(array_for_temperatures);
-        return array_for_temperatures;
-        
+       
     }
     catch(error)
     {
         console.log(2);
     } 
+}
+
+export async function temperatures()
+{
+    await takeParameters();
+    console.log(array_for_temperatures.length)
+    return array_for_temperatures;
 }
