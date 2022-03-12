2a4ef0eefae1fe0908f475f4f3edd8bcf5524693
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2022-03-12 10:24:28 +0200

    chart animations

--- fronend/animations.js
+++ fronend/animations.js
@@ -1,39 +1,57 @@
-var smooth_animations = anime.timeline
-(
-{
+var navigation = anime.timeline
+({
     easing: 'easeInOutSine',
-}
-)
+})
 
-smooth_animations.add
+navigation.add
 ({
     targets: ['.display-2'],
     translateY: [0, -20],
     opacity: [0,1],
     duration: 1000,
 })
+
 .add
 ({
     targets: '.--buttons-nav',
     opacity: [0,1],
-    duration: 2000,
+    duration: 1000,
 },'-=400')
+
 .add
 ({
     targets: '.--button-left',
     opacity: [0, 1],
     translateX: [-20,0],
 }, '-=1000')
+
 .add
 ({
     targets: '.--button-right',
     opacity: [0, 1],
     translateX: [20, 0],
 }, '-=1000')
-.add
+
+var charts = anime.timeline(
+{
+    easing: 'easeInOutSine',
+})
+
+charts.add
 ({
-    targets: '.--avg-values-right',
+    targets: '.chart1',
+    opacity: [0, 1],
+    translateY:[20, 0],
+    duration: 1500,
+    delay: 2000,
+})
+
+charts.add
+({
+    targets: '.--avg-values-right1',
     opacity: [0, 1],
     translateX: [90, 0],
-    delay: anime.stagger(200, {start: 0})
-}, '-=600')
+    delay: anime.stagger(200, {start: 0}),
+    easing: 'easeInOutExpo',
+    duration: 1500,
+}, '-=1500')
