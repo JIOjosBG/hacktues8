2d3b745605e26c5579fe35120e124391f881a50b
JIOjosBG <jjjooosssiiifff@gmail.com>
2022-03-12 11:43:03 +0200

    fixed cors for api

--- server/server/settings.py
+++ server/server/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'users',
     'measurements',
@@ -54,6 +55,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    '*',
 ]
 
 ROOT_URLCONF = 'server.urls'
